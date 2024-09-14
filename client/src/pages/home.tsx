e4780672ae89cc59e1087777c3ac65a54df76d5b
sreejithsree956266 <sreejithsree956266@gmail.com>
2024-09-14 05:26:53 +0000

    home page change

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function isAuth(){
+	const auth = Boolean(localStorage.getItem("token"))
+	return auth
+}
 
 const Home = () => {
-	return <div>Home</div>;
+    const [isLogged, setIsLogged] = useState(isAuth)
+
+	const logout = () => {
+		localStorage.removeItem("token")
+		setIsLogged(false)
+	}
+
+	return <div>
+		{
+			isLogged ? <div>
+				<h1>Wlcome back user</h1>
+				<button onClick={logout}>Logout</button>
+			</div> : <div>
+				<h1>Please authenticate</h1>
+				<Link to={"/login"}>Login</Link>
+				<Link to={"/signup"}>Signup</Link>
+			</div>
+		}
+	</div>
 };
 
 export default Home;
