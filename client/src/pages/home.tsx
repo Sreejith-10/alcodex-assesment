1bfb251b94237a74eacd6fb93cf3560e17bad77e
sreejith <sreejithsree956266@gmail.com>
2024-09-16 21:09:53 +0530

    home page change

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {useState} from "react";
 import {Link} from "react-router-dom";
 import {useToast} from "../hooks";
@@ -56,33 +55,6 @@ const Home = () => {
 			)}
 		</div>
 	);
-=======
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { isAuthenticated } from "../utils/isAuthenticated";
-
-
-const Home = () => {
-    const [isLogged, setIsLogged] = useState(isAuthenticated)
-
-	const logout = () => {
-		localStorage.removeItem("token")
-		setIsLogged(false)
-	}
-
-	return <div className="w-full h-[40dvh] flex items-center justify-center">
-		{
-			isLogged ? <div className="flex items-center flex-col gap-4">
-				<h1 className="text-3xl font-bold">Wlcome back user</h1>
-				<button className="bg-highlight text-white font-bold py-2 px-4 rounded" onClick={logout}>Logout</button>
-			</div> : <div>
-				<h1>Please authenticate</h1>
-				<Link to={"/login"}>Login</Link>
-				<Link to={"/signup"}>Signup</Link>
-			</div>
-		}
-	</div>
->>>>>>> 64ba2ff4560288b4ce6a325ba26f2a80205ab14b
 };
 
 export default Home;
